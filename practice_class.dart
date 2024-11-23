2d5303e8d8a6c8eeefead0693bd72fd4ce490393
dkdkdkdk555 <dkdkdkdk555@naver.com>
2024-11-23 22:51:02 +0900

    클래스 복습
    1. 같은 Unamed Constructor은 동시에 두가지가 쓰일 수 없다. 그러나 Named Constructor는 동시에 여러개도 쓰일 수 있다.
    2. 추상화 클래스는 상속받는것이고 기능의 틀만 제공하는것이 목적이기에 필드의 초기화를 직접 제공하진 않는다.
    그러나 추상화 클래스의 필드를 재정의하거나 super를 이용하여 간접적으로(?) 초기화 할 수 있다.
    또 추상화 클래스는 메소드의 구현을 강제한다.
    3. Cascade Notation : 중복제거 용도, 초기화와 필드값 성정을 동시에, 초기화와 기능사용을 동시에 하고 싶을때 사용하며, 체이닝 효과와 참조를 반복하지 않아도 되는 장점을 가지고 있다.
    4. Mixin 클래스는 프로퍼티 및 함수를 가지고 있다. 해당 값을 제공하는데 초점을 가지고 있으며 with키워드로 여러개의 mixin클래슬를 상속받을 수 있다. 프로퍼티의 재정의는 제공되지 않으나 getter를 통해 할 수 있긴한데 잘 안쓰는듯 하다.Mixin의 의미가 퇴색되기 때문. mxin에 정의된 필드는 상수로 초기화가 강제되고 final처럼 간주된다.

--- practice_class.dart
+++ practice_class.dart
@@ -1,19 +1,117 @@
 class Player{
+  /*
+    필드
+  */
   final String name;
   int xp, age;
   String team;
-  
-  Player(this.name, this.xp, this.age, this.team); // Positional
+  // 여까지만 쓰면 각 변수에 대해 반드시 초기화 되야한다고 뜬다.
+  // 또는 초기화 할 수 있는 수단(생성자)을 추가하기를 바란다.
 
-  /**
-   * Named{} Constructor
-   */
+  /*  
+    생성자
+    : 초기화의 수단으로서 생성자를 추가하면 필드에 컴파일 에러가 사라진다.
+  */
+  // Positional Constructor (Unnamed Constructor)
+  Player(this.name, this.xp, this.age, this.team);
+
+  // Named Constructor 인데 Unnamed Constructor 가 정의돼있으면 얘 정의 못함..ㅠ웃김
+  // Player({
+  //   required this.name, 
+  //   required this.xp, 
+  //   required this.team, 
+  //   required this.age});
+
+  // Named Constructor
   Player.createBluePlayer({required String name, required int age})
   : this.age = age,
     this.name = name,
     this.team = 'blue',
-    this.xp = 0;  
-}
-void main(){
+    this.xp = 0; // 디폴트값이 있는경우는 파라미터를 받지 않는다.
 
+  
+  void sayPlayerHi(){
+    print("Hi my name is $name and my age is $age");
+  }
+
+}
+
+/**
+ * 추상화 클래스
+ * : 다른 클래스들의 청사진
+ */
+abstract class Human {
+  String? ss;
+  void walk(); // 상속받으면 해당 메소드 재정의 하도록 강제
+}
+
+/**
+ * Enums
+ */
+enum Team {red, blue}
+
+class Player2 extends Human{
+  @override
+  void walk() {
+    print('im walkin');    
+  }
+
+  String name;
+  int xp;
+  Team team;
+
+  @override
+  String? ss;
+
+  Player2({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.ss,
+  });
+  
+}
+
+/**
+ * Mixins
+ * 생성자가 없는 클래스!
+ * 클래스에 프로퍼티를 추가하는 용도,
+ * 재사용성을 높임
+ * , 프로퍼티를 재정의 할 수 없다.
+ * mixin은 상수이고 final처럼 간주된다.
+ * Mixin은 기능(method) 제공에 초점이 맞춰져 있으며, 
+ * 필드 변경은 Mixin의 설계 철학에 맞지 않는 경우가 많다.
+ * getter로 재정의 할 수 있으나 잘 안쓸거같다.
+ */
+mixin class String {
+  final double strenghtLevel = 1500.00;
+}
+
+mixin class QuickRunner {
+  void runQuick(){
+    print("runnn!");
+  }
+}
+
+mixin class Tall {
+  final double height = 1.99;
+}
+
+class Horse with String, QuickRunner, Tall{
+}
+
+void main(){
+  /**
+   * Cascade Notation
+   * 중복제거 용도
+   * 참조를 반복하지 않아도 됨
+   * 체이닝 효과
+   * 
+   */
+  var nico = Player2(name: "ukha", xp: 12, team: Team.blue, ss: "11")
+  ..name = 'sieun'
+  ..xp = 14
+  ..team = Team.red;
+
+  print(nico.name);
 }
